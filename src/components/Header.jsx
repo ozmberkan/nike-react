d8ebbe983fd6445a704d913b252f0bdf86cfd006
ozmberkan <ozmberkan@gmail.com>
2024-05-12 23:05:26 +0300

    Drop show added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ export default function Header() {
   return (
     <div className="w-full h-[500px] bg-[url('./assets/aaabstract.png')] bg-opacity-40 bg-no-repeat bg-cover object-cover flex justify-start items-center relative pl-14">
       <div className="w-[1200px] font-semibold text-5xl flex flex-col gap-y-3 text-[#121212]">
-        <h1>Air Force</h1>
+        <h1 className="font-black">Air Force</h1>
         <p>Ustalıkla İşlenen İkonik ve Klasik Tasarım.</p>
       </div>
       <img
         src={shoe}
-        className="w-[600px] object-cover absolute right-0 top-0"
+        className="w-[600px] object-cover absolute right-0 top-0 drop-shadow-[0_35px_35px_rgba(0,0,0,0.30)] "
       />
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import shoe1 from "../assets/shoe1.png";
 import shoe2 from "../assets/shoe2.png";
 import shoe3 from "../assets/shoe3.png";
+import shoe4 from "../assets/shoe4.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,8 +14,8 @@ export default function Products() {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 3000,
-    autoplaySpeed: 5000,
+    speed: 800,
+    autoplaySpeed: 3000,
     cssEase: "linear",
   };
   return (
@@ -23,7 +24,7 @@ export default function Products() {
         {...settings}
         className="max-w-full h-full flex justify-center items-center"
       >
-        <div className=" w-full h-[550px] p-6 flex justify-center items-center my-auto">
+        <div className=" h-[550px] p-6 flex justify-center items-center">
           <div className="border-2 border-[#121212]/50 object-cover cursor-pointer rounded-xl  transition-all duration-500 flex justify-center items-center flex-col p-4">
             <h1 className="font-semibold text-4xl mt-5 text-[#121212]">
               Airforce Rainbow
@@ -51,6 +52,17 @@ export default function Products() {
               Airforce Blue
             </h1>
             <img src={shoe3} alt="" className="p-7" />
+            <button className="p-2 bg-[#121212] text-[#f1f1f1] rounded-full w-48 hover:bg-zinc-600 transition-colors duration-500 font-semibold text-lg">
+              Detay
+            </button>
+          </div>
+        </div>
+        <div className=" h-[550px] p-6">
+          <div className="border-2 border-[#121212]/50 object-cover cursor-pointer  rounded-xl  transition-all duration-500 flex justify-center items-center flex-col p-4">
+            <h1 className="font-semibold text-4xl mt-5 text-[#121212]">
+              Airforce Cherry
+            </h1>
+            <img src={shoe4} alt="" className="p-7" />
             <button className="p-2 bg-[#121212] text-[#f1f1f1] rounded-full w-48 hover:bg-zinc-600 transition-colors duration-500 font-semibold text-lg">
               Detay
             </button>
