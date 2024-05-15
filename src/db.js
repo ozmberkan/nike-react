75577e138847fab08b1ad1df88bf24a37ffd8d50
berkanozm <ozmberkan@gmail.com>
2024-05-15 18:09:00 +0300

    Check

--- src/db.js
+++ src/db.js
@@ -2,22 +2,22 @@ const shoes = {
   airforcerainbow: {
     id: 1,
     name: "AirForce Rainbow",
-    image: "/src/assets/shoe1.png",
+    image: "/assets/shoe1.png",
   },
   airforceone: {
     id: 2,
     name: "AirForce One",
-    image: "/src/assets/shoe2.png",
+    image: "/assets/shoe2.png",
   },
   airforceblue: {
     id: 3,
     name: "AirForce Blue",
-    image: "/src/assets/shoe3.png",
+    image: "/assets/shoe3.png",
   },
   airforcecherry: {
     id: 4,
     name: "AirForce Cherry",
-    image: "/src/assets/shoe4.png",
+    image: "/assets/shoe4.png",
   },
 };
 
