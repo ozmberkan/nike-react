6ba9a1092101d84258f9e5ac37870970809a159b
berkanozm <ozmberkan@gmail.com>
2024-05-15 15:13:33 +0300

    check href

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
 
   useEffect(() => {
-    axios.get('https://fakestoreapi.com/products')
+    axios.get('src/db.json')
       .then((response) => {
         setData(response.data);
       })
