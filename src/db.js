60892047e9156730015a4361f088e8ae656dfa06
berkanozm <ozmberkan@gmail.com>
2024-05-15 18:22:21 +0300

    check

--- src/db.js
+++ src/db.js
@@ -2,17 +2,17 @@ const shoes = {
   airforcerainbow: {
     id: 1,
     name: "AirForce Rainbow",
-    image: "/dist/assets/bannerofhistory-C5au-8vt.svg",
+    image: "dist/assets/bannerofhistory-C5au-8vt.svg",
   },
   airforceone: {
     id: 2,
     name: "AirForce One",
-    image: "/dist/assets/Historyshoe-B4NE58KA.png",
+    image: "dist/assets/Historyshoe-B4NE58KA.png",
   },
   airforceblue: {
     id: 3,
     name: "AirForce Blue",
-    image: "/dist/assets/Shoes-CWD2kgmM.png",
+    image: "dist/assets/Shoes-CWD2kgmM.png",
   },
   airforcecherry: {
     id: 4,
